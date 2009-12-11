ece6a62f475a88d4c93a26b9506032ad94edd4f2
Florian Munz <surf@theflow.de>
2009-12-11 15:05:40 +0100

    make the importer work again with new format

--- import.rb
+++ import.rb
@@ -29,8 +29,13 @@ exceptions.compact.each do |e|
   id = next_exception_id
   key = Digest::SHA1.hexdigest(['controller_name', 'action_name', 'exception_class'].map { |k| e[k] }.join(':'))
 
+  e['cgi_data'] = e.delete('environment')
+  e.delete('framework')
+  e.delete('application_root')
+  e.delete('language')
+
   # store the exception data
-  redis.set("Exceptionist::Occurrence:#{id}", Yajl::Encoder.encode(e.merge('id' => id, 'uber_key' => key)))
+  redis.set("Exceptionist::Occurrence:id:#{id}", Yajl::Encoder.encode(e.merge('id' => id, 'uber_key' => key)))
 
   # every uber exception has a list of occurences
   redis.push_tail("Exceptionist::UberException:#{key}", id)
