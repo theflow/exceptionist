f5eb2f88368f2fb760cf7e311317fb94c84694fd
Florian Munz <surf@theflow.de>
2010-01-29 17:43:31 +0100

    some simple data managing scripts

--- export.rb
+++ export.rb
@@ -19,13 +19,15 @@ module Exceptionist
   end
 end
 
-occurrences = Yajl::Parser.parse(File.read('occurrences_export.json'))
 
-occurrences.each do |occurrence_hash|
-  occurrence_hash.delete('uber_key')
-  occurrence = Occurrence.new(occurrence_hash)
-  occurrence.save
+occurrence_keys = Exceptionist.redis.keys("Exceptionist::Occurrence:id:*")
 
-  UberException.occurred(occurrence)
+occurrences = occurrence_keys.map do |key|
+  id = key.split(':').last
+  Occurrence.find(id)
 end
-# 
+
+File.open('occurrences_export.json', 'w') do |file|
+  file.write(Yajl::Encoder.encode(occurrences))
+end
+
