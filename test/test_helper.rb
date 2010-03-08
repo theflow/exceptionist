2da0ea5dfaa23c973cea393b60c63ca5469e6c51
Florian Munz <surf@theflow.de>
2010-03-09 00:16:51 +0100

    new version of test/spec/mini

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -32,8 +32,8 @@ Exceptionist.redis = Redis.new(:host => '127.0.0.1', :port => 9736)
 
 
 ##
-# test/spec/mini 3
-# http://gist.github.com/25455
+# test/spec/mini 5
+# http://gist.github.com/307649
 # chris@ozmm.org
 #
 def context(*args, &block)
@@ -41,10 +41,15 @@ def context(*args, &block)
   require 'test/unit'
   klass = Class.new(defined?(ActiveSupport::TestCase) ? ActiveSupport::TestCase : Test::Unit::TestCase) do
     def self.test(name, &block)
-      define_method("test_#{name.gsub(/\W/,'_')}", &block) if block
+      define_method("test_#{name.to_s.gsub(/\W/,'_')}", &block) if block
     end
     def self.xtest(*args) end
-    def self.setup(&block) define_method(:setup, &block) end
+    def self.context(*args, &block) instance_eval(&block) end
+    def self.setup(&block)
+      define_method(:setup) { self.class.setups.each { |s| instance_eval(&s) } }
+      setups << block
+    end
+    def self.setups; @setups ||= [] end
     def self.teardown(&block) define_method(:teardown, &block) end
   end
   (class << klass; self end).send(:define_method, :name) { name.gsub(/\W/,'_') }
