75c3b583bbecd3005bf6ba60c9e5bff56ffa87e8
Florian Munz <surf@theflow.de>
2010-04-21 23:35:13 +0200

    move Exceptionist module into separate file

--- boot.rb
+++ boot.rb
@@ -9,54 +9,7 @@ require 'models/project'
 require 'models/uber_exception'
 require 'models/occurrence'
 
-
-module Exceptionist
-  def self.namespace
-    "Exceptionist"
-  end
-
-  def self.redis
-    @redis ||= Redis.new(:host => host, :port => port, :thread_safe => true)
-  end
-
-  def self.redis=(server)
-    case server
-    when String
-      host, port = server.split(':')
-      @redis = Redis.new(:host => host, :port => port, :thread_safe => true)
-    when Redis
-      @redis = server
-    else
-      raise "I don't know what to do with #{server.inspect}"
-    end
-  end
-
-  def self.config
-    @config ||= {}
-  end
-
-  def self.filter
-    @filter ||= FilterStore.new
-  end
-
-  class FilterStore
-    def initialize
-      @filters = []
-    end
-
-    def add(name, &block)
-      @filters << [name, block]
-    end
-
-    def clear
-      @filters = []
-    end
-
-    def all
-      @filters
-    end
-  end
-end
+require 'models/exceptionist'
 
 
 begin
