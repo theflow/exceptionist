13d0604cd0503af8fa8b59b87f6c2776ea6401d4
Florian Munz <surf@theflow.de>
2010-05-19 22:18:44 +0200

    whitespace cleanup

--- tools.rb
+++ tools.rb
@@ -9,38 +9,36 @@ module Exceptionist
         id = key.split(':').last
         Occurrence.find(id)
       end
-
       occurrences = occurrences.sort_by(&:occurred_at)
 
-	  key_groups = []
-	  occurrences.each_slice(10000) { |group| key_groups << group }
-	  key_groups.each_with_index do |keys, i|
-		puts "exporting #{i}"
+      key_groups = []
+      occurrences.each_slice(10000) { |group| key_groups << group }
+      key_groups.each_with_index do |keys, i|
+        puts "exporting #{i}"
 
-      File.open("occurrences_export_#{i}.json", 'w') do |file|
-        file.write(Yajl::Encoder.encode(keys))
+        File.open("occurrences_export_#{i}.json", 'w') do |file|
+          file.write(Yajl::Encoder.encode(keys))
+        end
       end
-	  end
     end
   end
 
   class Importer
     def self.run
-	  files = Dir.glob('occurrences_export*').sort
-	  files.each do |file|
-		puts "importing #{file}"
+      files = Dir.glob('occurrences_export*').sort
+      files.each do |file|
+        puts "importing #{file}"
 
-      occurrences = Yajl::Parser.parse(File.read(file))
+        occurrences = Yajl::Parser.parse(File.read(file))
+        occurrences.each do |occurrence_hash|
+          occurrence_hash.delete('uber_key')
+          occurrence_hash.delete('id')
+          occurrence = Occurrence.new(occurrence_hash)
+          occurrence.save
 
-      occurrences.each do |occurrence_hash|
-        occurrence_hash.delete('uber_key')
-        occurrence_hash.delete('id')
-        occurrence = Occurrence.new(occurrence_hash)
-        occurrence.save
-
-        UberException.occurred(occurrence)
+          UberException.occurred(occurrence)
+        end
       end
-	  end
     end
   end
 
