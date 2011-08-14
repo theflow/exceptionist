ff1f686393381cdc4e382e18d4afefac9a6c7422
Florian Munz <surf@theflow.de>
2011-08-14 16:42:02 +0200

    moar indexes

--- lib/tools.rb
+++ lib/tools.rb
@@ -61,8 +61,11 @@ module Exceptionist
 
   class IndexCreator
     def self.run
+      Exceptionist.mongo['exceptions'].ensure_index(:project_name)
+
       Exceptionist.mongo['occurrences'].ensure_index([[:occurred_at_day, Mongo::ASCENDING], [:project_name, Mongo::ASCENDING]])
       Exceptionist.mongo['occurrences'].ensure_index(:uber_key)
+      Exceptionist.mongo['occurrences'].ensure_index([[:uber_key, Mongo::ASCENDING], [:occurred_at, Mongo::ASCENDING]])
     end
   end
 end
