858f4ff98dbb9c3cf5d92ee88609a2f75fb9c192
Simon Mulser <simonmulser@gmail.com>
2014-09-11 13:46:04 +0200

    fix wrong name of keyword argument

--- lib/models/uber_exception.rb
+++ lib/models/uber_exception.rb
@@ -82,7 +82,7 @@ class UberException
   end
 
   def current_occurrence(position)
-    occurrences = Occurrence.find(uber_key: id, sort: { occurred_at: { order: 'asc'} }, position: position - 1, size: 1)
+    occurrences = Occurrence.find(uber_key: id, sort: { occurred_at: { order: 'asc'} }, from: position - 1, size: 1)
     occurrences.any? ? occurrences.first : nil
   end
 
