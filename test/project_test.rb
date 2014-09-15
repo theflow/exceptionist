da5cca89290b4c7946ec5875513ec76a99eb6aa3
Simon Mulser <simonmulser@gmail.com>
2014-09-15 11:17:37 +0200

    add test for project

--- test/project_test.rb
+++ test/project_test.rb
@@ -7,6 +7,13 @@ class ProjectTest < AbstractTest
     assert_equal 4, Project.last_n_days(4).size
   end
 
+  def test_exceptions_count
+    project = Project.new("ExampleProject")
+
+    assert_equal 0, project.exceptions_count
+  end
+
+
   def test_last_thirty_days
     project = Project.new('ExampleProject')
 
@@ -19,6 +26,20 @@ class ProjectTest < AbstractTest
     Exceptionist.esclient.refresh
 
     assert_equal 1, project.last_thirty_days.last[1]
+  end
+
+  def test_last_deploy
+    project = Project.new("ExampleProject")
+
+    assert_equal nil, project.last_deploy
+  end
+
+  def test_find_by_key
+    assert_equal nil, Project.find_by_key('test_key')
+
+    Exceptionist.add_project('project', 'test_key')
+
+    assert_equal 'project', Project.find_by_key('test_key').name
   end
 
   def test_find_by_key
