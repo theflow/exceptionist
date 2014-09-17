e1ad77ae195118317e7ad6d362a693b08eb0f14c
Simon Mulser <simonmulser@gmail.com>
2014-09-17 11:33:17 +0200

    move mapping into a yaml file

--- lib/es_helper.rb
+++ lib/es_helper.rb
@@ -10,39 +10,8 @@ module ESHelper
       rescue Elasticsearch::Transport::Transport::Errors::NotFound
       end
 
-      occurr_prop =
-          {action_name: { type: 'string', index: 'not_analyzed' },
-           controller_name: { type: 'string', index: 'not_analyzed' },
-           project_name: { type: 'string', index: 'not_analyzed' },
-           uber_key: { type: 'string', index: 'not_analyzed' },
-           exception_class: { type: 'string', index: 'not_analyzed' },
-           occurred_at_day: { type: 'date' },
-           occurred_at: { type: 'date' } }
-
-      Exceptionist.esclient.create_indices('exceptionist',
-                                           { mappings: {
-                                               _default_: {
-                                                   dynamic: 'false'
-                                               },
-                                               occurrences: { properties: occurr_prop },
-                                               exceptions:{
-                                                   properties: {
-                                                       project_name: { type: 'string', index: 'not_analyzed' },
-                                                       closed: { type: 'boolean' },
-                                                       last_occurrence: { properties: occurr_prop},
-                                                       first_occurred_at: { type: 'date' },
-                                                       occurrences_count: {type: 'long'}
-                                                   } },
-                                               deploys: {
-                                                   properties: {
-                                                       project_name: { type: 'string', index: 'not_analyzed' },
-                                                       version: { type: 'string', index: 'not_analyzed' },
-                                                       deploy_time: { type: 'date' },
-                                                       changelog_link: { type: 'string', index: 'not_analyzed'}
-                                                   } },
-                                           } })
+      Exceptionist.esclient.create_indices('exceptionist',YAML.load(File.read('lib/mapping.yaml')))
       Exceptionist.esclient.refresh
     end
   end
-  
 end
